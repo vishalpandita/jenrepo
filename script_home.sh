11c16a497cfd6681c003be76317633a3fcd2d403
vishalpandita <36498511+vishalpandita@users.noreply.github.com>
2021-10-16 15:18:14 +0530

    Create script_home.sh

---
+++ script_home.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Jenkins Configuration Directory
+cd $JENKINS_HOME
+
+#Add general configuration, job configurations, and user content
+git add -- *.xml jobs/*/*.xml userContent/* ansible/*
+
+#only add user configurations if they exist
+if [ -d users ]; then
+	user_configs=`ls users/*/config.xml`
+	
+
+	if [ -n "$user_configs" ]; then
+		git add $user_configs
+	fi
+fi
+
+# mark as deleted anything that's been, well, deleted
+to_remove=`git status | grep "deleted" | awk '{print $3}'`
+
+if [ -n "$to_remove" ]; then
+	git rm --ignore-unmatch $to_remove
+fi
+
+git commit -m "Automated Jenkins commit"
+
+git push -q -u origin master
